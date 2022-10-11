d2f93e4141e7e94d06884090472879f44a6aa839
Csegezi Noemi Szidonia <115534797+noemiszidonia@users.noreply.github.com>
2022-10-11 11:33:22 +0300

    Update streamlit_app.py
    Commit breakfast items - extended

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega3')
-streamlit.text('Spinach Smoothie')
-streamlit.text('Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
